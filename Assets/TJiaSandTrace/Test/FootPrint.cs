eaf20ad473e7fbdd59c05cd17e2058f03fc4bf62
赵佩豪 <peihao.zhao@centurygame.com>
2020-12-08 12:22:50 +0800

    角色移动版

---
+++ Assets/TJiaSandTrace/Test/FootPrint.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class FootPrint : MonoBehaviour
+{
+    private const string FootPrintShaderName = "WWFramework/FootPrintGenerator";
+    private enum FootPrintPass
+    {
+        Generator = 0,
+        Clear = 1,
+    }
+
+    /// <summary>
+    /// 脚印法线贴图，A通道是影响范围，0为不影响
+    /// </summary>
+    [SerializeField] private Texture _footPrintBump;
+    [SerializeField] private float _footPrintBumpScale = 8; 
+    
+    [SerializeField] private float _footPrintSize = 1;
+    [SerializeField] private int _worldSize = 20;
+    [SerializeField] private int _textureSize = 256;
+
+    [SerializeField] private float _footPrintAtten = 0.1f;
+    [SerializeField] private float _duration = 0.1f;
+    private float _curDuration = 0;
+
+    /// <summary>
+    /// 上一帧的结果
+    /// RGB通道是法线
+    /// </summary>
+    [SerializeField] private RenderTexture _srcRenderTexture;
+    [SerializeField] private RenderTexture _dstRenderTexture;
+    
+    private Material _footPrintMaterial;
+    private Transform _cacheTransform;
+    private Vector3 _lastWorldPosition;
+
+    private void OnEnable()
+    {
+        _footPrintMaterial = new Material(Shader.Find(FootPrintShaderName));
+
+        _srcRenderTexture = RenderTexture.GetTemporary(_textureSize, _textureSize, 0, RenderTextureFormat.ARGB32,
+            RenderTextureReadWrite.Linear, 1);
+        _srcRenderTexture.wrapMode = TextureWrapMode.Clamp;
+        _srcRenderTexture.filterMode = FilterMode.Point;
+        Graphics.Blit(null, _srcRenderTexture, _footPrintMaterial, (int)FootPrintPass.Clear);
+        
+        _dstRenderTexture = RenderTexture.GetTemporary(_srcRenderTexture.descriptor);
+        Shader.SetGlobalTexture("_FootPrintTrace", _dstRenderTexture);
+        
+        Shader.SetGlobalTexture("_FootPrintBump", _footPrintBump);
+
+        _cacheTransform = transform;
+        _lastWorldPosition = transform.position;
+        _curDuration = 0;
+    }
+
+    private void OnDisable()
+    {
+        RenderTexture.ReleaseTemporary(_srcRenderTexture);
+        _srcRenderTexture = null;
+        
+        RenderTexture.ReleaseTemporary(_dstRenderTexture);
+        _dstRenderTexture = null;
+        
+        _footPrintMaterial = null;
+    }
+
+    private void LateUpdate()
+    {
+        if (_footPrintMaterial == null)
+        {
+            return;
+        }
+
+        _curDuration += Time.deltaTime;
+        if (_curDuration < _duration)
+        {
+            return;
+        }
+        var curPos = _cacheTransform.position;
+
+        Shader.SetGlobalFloat("_FootPrintBumpScale", _footPrintBumpScale);
+        Shader.SetGlobalFloat("_FootPrintSize", _footPrintSize);
+        Shader.SetGlobalInt("_WorldSize", _worldSize);
+        Shader.SetGlobalFloat("_FootPrintAtten", _footPrintAtten * _curDuration);
+        Shader.SetGlobalVector("_WorldPosition", curPos);
+        Shader.SetGlobalVector("_DeltaWorldPosition", _lastWorldPosition - curPos);
+        Shader.SetGlobalTexture("_FootPrintTex", _dstRenderTexture);
+
+        Graphics.Blit(_srcRenderTexture, _dstRenderTexture, _footPrintMaterial, (int)FootPrintPass.Generator);
+        Graphics.Blit(_dstRenderTexture, _srcRenderTexture);
+        
+        _curDuration = 0;
+        _lastWorldPosition = curPos;
+    }
+}
---
+++ Assets/TJiaSandTrace/Test/FootPrintGenerator.shader
@@ -0,0 +1,121 @@
+﻿Shader "WWFramework/FootPrintGenerator"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque"
+        }
+        LOD 100
+
+        Pass //Trace Generation
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            sampler2D _FootPrintBump;
+
+            half _FootPrintSize;
+            half _WorldSize;
+            half _FootPrintAtten;
+            float3 _WorldPosition;
+            float3 _DeltaWorldPosition;
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                float3 deltaPos = _DeltaWorldPosition;
+                float2 deltaUV = deltaPos.xz / _WorldSize;
+                float2 lastUV = i.uv - deltaUV;
+                // 上一步的信息
+                half4 lastPrintTrace = tex2D(_MainTex, lastUV);
+                // 边缘计算
+                half2 lastFootPrints = step(0, lastUV) * step(lastUV, 1);
+                lastPrintTrace *= lastFootPrints.x * lastFootPrints.y;
+                // 脚印变浅
+                lastPrintTrace.a -= _FootPrintAtten;
+
+                // 算出该点在脚印上的uv
+                float2 uv = (i.uv - 0.5) * _WorldSize / _FootPrintSize + 0.5;
+                // 暂不考虑朝向
+                half4 footPrint = tex2D(_FootPrintBump, uv);
+                // 反向脚印的法线，即将xy反向，可以推导出来
+                footPrint.rg = 1 - footPrint.rg;
+
+                half4 col = footPrint;
+                // 深的位置决定最后的效果
+                half stepDeep = step(lastPrintTrace.a, col.a);
+                col.a = max(lastPrintTrace.a, col.a);
+                col.rgb = col.rgb * stepDeep + lastPrintTrace.rgb * (1 - stepDeep);
+
+                // 边缘检测
+                half2 uvCheck = step(0.01, i.uv) * step(i.uv, 0.99);
+                col.a *= uvCheck.x * uvCheck.y;
+
+                return col;
+            }
+            ENDCG
+        }
+
+        Pass //Init
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+            };
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                return 0;
+            }
+            ENDCG
+        }
+    }
+}
